4ca87c010e65d6ea55ca2780938a9097c3a25286
SMControl <143865768+SMControl@users.noreply.github.com>
2025-05-18 08:19:15 +0100

    Version 1.52
    updated firebird installer to the ua module instead of the standalone - better for being called

--- smt.ps1
+++ smt.ps1
@@ -1,10 +1,10 @@
-Write-Host "smt.ps1 - Version 1.51"
+Write-Host "smt.ps1 - Version 1.52"
 
 # Define task names and URLs
 $task1Name = "SO Upgrade Assistant"
 $task1Url = "https://raw.githubusercontent.com/SMControl/SO_Upgrade/refs/heads/main/main/soua.ps1"
 $task2Name = "SM Firebird Installer"
-$task2Url = "https://raw.githubusercontent.com/SMControl/SM_Firebird_Installer/main/SMFI_Online.ps1"
+$task2Url = "https://raw.githubusercontent.com/SMControl/SO_Upgrade/refs/heads/main/modules/module_firebird.ps1"
 $task3Name = "SM Tasks (Testing)"
 $task3Url = "https://your-smservices.com/SM_Services.ps1"
 $task4Name = "PDTWifi64 Upgrade"
