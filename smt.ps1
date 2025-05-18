dfe456f363b7b75e0af0fdc3a099daefd88b5847
SMControl <143865768+SMControl@users.noreply.github.com>
2025-05-18 21:32:08 +0100

    smt.ps1 - Version 1.51
    sm-tasks out of testing, added its url

--- smt.ps1
+++ smt.ps1
@@ -1,15 +1,15 @@
-Write-Host "smt.ps1 - Version 1.52"
+Write-Host "smt.ps1 - Version 1.51"
 
 # Define task names and URLs
 $task1Name = "SO Upgrade Assistant"
 $task1Url = "https://raw.githubusercontent.com/SMControl/SO_Upgrade/refs/heads/main/main/soua.ps1"
 $task2Name = "SM Firebird Installer"
-$task2Url = "https://raw.githubusercontent.com/SMControl/SO_Upgrade/refs/heads/main/modules/module_firebird.ps1"
-$task3Name = "SM Tasks (Testing)"
-$task3Url = "https://your-smservices.com/SM_Services.ps1"
-$task4Name = "PDTWifi64 Upgrade"
+$task2Url = "https://raw.githubusercontent.com/SMControl/SM_Firebird_Installer/main/SMFI_Online.ps1"
+$task3Name = "SM Scheduled Tasks"
+$task3Url = "https://raw.githubusercontent.com/SMControl/SM_Tasks/refs/heads/main/SM_Tasks.ps1"
+$task4Name = "PDTWifi Upgrade"
 $task4Url = "https://raw.githubusercontent.com/SMControl/smt/refs/heads/main/modules/PDTWifi64_Upgrade.ps1"
-$task5Name = "Setup new PC (Testing)"
+$task5Name = "PC Transfer (Testing)"
 $task5Url = "https://raw.githubusercontent.com/SMControl/smpc/refs/heads/main/smpc.ps1"
 
 
